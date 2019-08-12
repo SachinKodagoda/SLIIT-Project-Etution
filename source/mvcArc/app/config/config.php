4437e89966a4ec975ef78a2565c5c84923e1d68f
SachinKodagoda <duminda.g.k@gmail.com>
2019-08-12 23:20:53 +0530

    server link added

--- source/mvcArc/app/config/config.php
+++ source/mvcArc/app/config/config.php
@@ -1,4 +1,6 @@
 <?php
+
+// Local Host
 define("DB_HOST", "localhost");
 define("DB_USER", "root");
 define("DB_PASS", "");
@@ -8,3 +10,16 @@ define('APPROOT', dirname(dirname(__FILE__)));
 define('URLROOT', 'http://localhost:3000/mvcArc');
 define('SITENAME', 'MyMVC');
 define('APPVERSION', '1.0.0');
+
+
+// Remort Host
+
+// define("DB_HOST", "localhost");
+// define("DB_USER", "u820269341_sachi");
+// define("DB_PASS", "Vlviq5tNWGG7");
+// define("DB_NAME", "u820269341_iwt");
+
+// define('APPROOT', dirname(dirname(__FILE__)));
+// define('URLROOT', 'https://wda-soft.com/mvcArc');
+// define('SITENAME', 'MyMVC');
+// define('APPVERSION', '1.0.0');
